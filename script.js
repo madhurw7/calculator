8cf03d6596aa7ea85657c363d6a2b9af61eb7e25
madhurw7 <madhurw7official@gmail.com>
2017-09-03 03:16:14 +0530

    Finished Calculator

--- script.js
+++ script.js
@@ -1,11 +1,142 @@
-var but1 = document.getElementById("but1");
-var but2 = document.getElementById("but2");
-var but3 = document.getElementById("but3");
-var but4 = document.getElementById("but4");
-var but5 = document.getElementById("but5");
-var but6 = document.getElementById("but6");
-var but7 = document.getElementById("but7");
-var but8 = document.getElementById("but8");
-var but9 = document.getElementById("but9");
-var but0 = document.getElementById("but0");
-var butAdd = document.getElementById("butAdd");
+
+
+//function startEventHandlers(){
+    var but = [];
+    var i = 0;
+    for(i = 0; i < 11; i++){
+        but[i] = document.getElementById("but" + i);
+    }
+    var butAdd = document.getElementById("butAdd");
+    var butMinus = document.getElementById("butMinus");
+    var butMultiply = document.getElementById("butM");
+    var butResult = document.getElementById("butResult");
+    var butDivide = document.getElementById("butDivide");
+    var butClear = document.getElementById("butClear");
+    var displayArea = document.getElementById("displayArea");
+
+    function clearDisplay(){
+        if (displayArea.value == "Everything appears here")
+        displayArea.value = "";
+    }
+    
+    function butValue(x) {
+        displayArea.value = displayArea.value + x;
+        
+    }
+    
+   // document.onclick = function() {
+     //   alert("What");
+    //};
+    
+    displayArea.onfocus = function(){
+        clearDisplay();
+    }; 
+    
+    
+    
+    //Button Clicking Event Handlers
+    but[0].onclick = function() {
+        clearDisplay();
+        butValue("0");
+        return false;
+    }
+    
+    but[1].onclick = function() {
+        clearDisplay();
+        butValue("1");
+        return false;
+    }
+    
+    but[2].onclick = function() {
+        clearDisplay();
+        butValue("2");
+        return false;
+    }
+    
+    but[3].onclick = function() {
+        clearDisplay();
+        butValue("3");
+        return false;
+    }
+    
+    but[4].onclick = function() {
+        clearDisplay();
+        butValue("4");
+        return false;
+    }
+    
+    but[5].onclick = function() {
+        clearDisplay();
+        butValue("5");
+        return false;
+    }
+    
+    but[6].onclick = function() {
+        clearDisplay();
+        butValue("6");
+        return false;
+    }
+    
+    but[7].onclick = function() {
+        clearDisplay();
+        butValue("7");
+        return false;
+    }
+    
+    but[8].onclick = function() {
+        clearDisplay();
+        butValue("8");
+        return false;
+    }
+    
+    but[9].onclick = function() {
+        clearDisplay();
+        butValue("9");
+        return false;
+    }
+    
+    butAdd.onclick = function() {
+        clearDisplay();
+        butValue("+ ");
+        return false;
+    }
+    
+    butMinus.onclick = function() {
+        clearDisplay();
+        butValue("- ");
+        return false;
+    }
+    
+    butResult.onclick = function() {
+        var res = eval(displayArea.value);
+        document.getElementById("result").innerHTML = res.toString();
+        return false;
+    }
+    
+     butClear.onclick = function() {
+        displayArea.value = "";
+        return false;
+    }
+    
+    
+    
+    butDivide.onclick = function() {
+        clearDisplay();
+        butValue("/ ");
+        return false;
+    }
+    
+    butMultiply.onclick = function() {
+        clearDisplay();
+        butValue("* ");
+        return false;
+    }
+    
+   
+    
+    
+//}
+
+/*window.onload = function() {
+    startEventHandlers();
+};*/
